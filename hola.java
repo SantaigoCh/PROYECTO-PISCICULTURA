18f27fe790f3404813d365a93f3709b9a60fcd31
crixlope <cristian.burgos2005@gmail.com>
2025-04-08 13:05:39 -0500

    hola.java

---
+++ hola.java
@@ -0,0 +1,12 @@
+public class SumTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 5; // Primer número
+        int num2 = 10; // Segundo número
+        
+        // Sumar los dos números
+        int suma = num1 + num2;
+        
+        // Imprimir el resultado
+        System.out.println("La suma de " + num1 + " y " + num2 + " es: " + suma);
+    }
+}

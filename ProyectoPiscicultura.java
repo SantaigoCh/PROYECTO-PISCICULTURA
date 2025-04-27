5ac997db6b7db0fed19b02ff0ce1773c5fe60334
SantaigoCh <123280244+SantaigoCh@users.noreply.github.com>
2025-04-26 21:13:02 -0500

    Update ProyectoPiscicultura.java

--- ProyectoPiscicultura.java
+++ ProyectoPiscicultura.java
@@ -1,6 +1,5 @@
 /* Este será el main del proyecto de piscicultura trabajado en Java, La carpeta principal del proyecto tiene el nombre de ProyectoPiscicultura,
 el motor de bases de datos trabajado será PostgreSQL. */
-
 package com.mycompany.proyectopiscicultura;
 
 import java.util.Scanner;
@@ -14,18 +13,25 @@ public class ProyectoPiscicultura {
         do {
             System.out.println("\n==== Sistema de Piscicultura ====");
             System.out.println("1. Registrarse");
-            System.out.println("2. Iniciar Sesión");
+            System.out.println("2. Iniciar Sesion");
             System.out.println("3. Salir");
-            System.out.print("Seleccione una opción: ");
+            System.out.print("Seleccione una opcion: ");
             opcion = sc.nextInt();
             sc.nextLine(); 
 
             switch (opcion) {
                 case 1 -> MenuRegistro.mostrarMenuRegistro();
-                case 2 -> MenuLogin.mostrarMenuLogin();
+                
+                case 2 -> {
+                    MenuLogin.mostrarMenuLogin();
+                }
+                
                 case 3 -> System.out.println("¡Hasta luego!");
-                default -> System.out.println("Opción no válida.");
+
+                default -> System.out.println("Opcion no válida.");
             }
         } while (opcion != 3);
+
+        sc.close();
     }
 }

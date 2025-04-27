c24ea9d2ff01467cc50c9a710c2a6373b54692dc
SantaigoCh <123280244+SantaigoCh@users.noreply.github.com>
2025-04-26 21:20:45 -0500

    Delete hola.java

--- hola.java
+++
@@ -1,8 +0,0 @@
-public class SumTwoNumbers {
-    public static void main(String[] args) {
-        int num1 = 5; 
-        int num2 = 10; 
-        int suma = num1 + num2;
-        System.out.println("La suma de " + num1 + " y " + num2 + " es: " + suma);
-    }
-}
